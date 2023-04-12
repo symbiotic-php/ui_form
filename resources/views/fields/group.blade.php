6f705030bf1c349ef222eca344bdd17d513fb016
Sergey Surkov <dissonancephp@gmail.com>
2023-04-13 00:24:43 +0300

    support 8.2+

--- resources/views/fields/group.blade.php
+++ resources/views/fields/group.blade.php
@@ -1,6 +1,14 @@
 <fieldset>
     <legend>{{$group->getTitle()}}</legend>
-    @foreach($group->getFields() as $v)
-        {!! $v->render() !!}
-    @endforeach
+    @if($group->isMulti())
+        <div class="multigroup">
+            @foreach($group->getSubGroups() as $v)
+                {!! $v->render() !!}
+            @endforeach
+        </div>
+    @else
+        @foreach($group->getFields() as $v)
+            {!! $v->render() !!}
+        @endforeach
+    @endif
 </fieldset>
